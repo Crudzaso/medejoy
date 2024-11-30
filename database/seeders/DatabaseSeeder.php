985e0c6214f0bf88de9f1787987a780117621dbf
alejandroava <alejandroalzate.ava@gmail.com>
2024-11-29 23:45:19 -0500

    feat:tables draws,raffles,lotteries populated in database with factories and seeders using faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
         // $this->call(LotterySeeder::class);
         // Raffle::factory(2)->create();
-        Draws::factory(2)->create();
+        // Draws::factory(2)->create();
 
         // User::factory()->create([
         //     'name' => 'Test User',
